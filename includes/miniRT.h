6d03429a2710986223b281abf8ff59cd2c8346ba
shorei <160503699+gao0325ts@users.noreply.github.com>
2025-08-22 13:41:35 +0900

    [feat] struct.hを追加し、構造体を更新

--- includes/miniRT.h
+++ includes/miniRT.h
@@ -1,20 +1,23 @@
-#ifndef	MINI_RT_H
-# define MINI_RT_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   miniRT.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/22 13:37:02 by stakada           #+#    #+#             */
+/*   Updated: 2025/08/22 13:37:30 by stakada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "libft.h"
-#include "mlx.h"
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include "libft.h"
+# include "mlx.h"
+# include "struct.h"
 
 # define WIN_WIDTH 1080
 # define WIN_HEIGHT 800
-
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
-int	main(int argc, char **argv);
 
 #endif
