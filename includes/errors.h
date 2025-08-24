1eda9c06492a88ea978104bac894bb2993ed50cf
shorei <160503699+gao0325ts@users.noreply.github.com>
2025-08-24 16:36:17 +0900

    [refactor] 不要なincludeの削除

--- includes/errors.h
+++ includes/errors.h
@@ -6,14 +6,12 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 19:42:02 by stakada           #+#    #+#             */
-/*   Updated: 2025/08/24 16:32:59 by stakada          ###   ########.fr       */
+/*   Updated: 2025/08/24 16:34:13 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERRORS_H
 # define ERRORS_H
-
-# include "config.h"
 
 # define ERR_MSG_USAGE "Usage: %s scene_file.rt"
 # define ERR_MSG_EXT "File must have a %s extension"
