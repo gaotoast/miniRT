bc28ee0c422f5250ea325c0d25aef24b7c70a5d2
shorei <160503699+gaotoast@users.noreply.github.com>
2026-02-09 18:07:28 +0900

    [fix] colorパース時のdouble→int変換 Fixes #5

--- srcs/parsing/parse_utils.c
+++ srcs/parsing/parse_utils.c
@@ -3,14 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   parse_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kinamura <kinamura@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 13:13:12 by stakada           #+#    #+#             */
-/*   Updated: 2025/12/30 14:52:50 by kinamura         ###   ########.fr       */
+/*   Updated: 2026/02/09 18:07:09 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
+
+static int	get_int(char *str, int *n)
+{
+	char	*endptr;
+	long	ret;
+
+	if (!str || !*str || !n)
+		return (-1);
+	endptr = NULL;
+	ret = ft_strtol(str, &endptr, 10);
+	if (endptr && !(*endptr == '\0' || *endptr == '\n'))
+		return (-1);
+	if (ret < (long)INT_MIN || ret > (long)INT_MAX)
+		return (-1);
+	*n = (int)ret;
+	return (0);
+}
 
 int	get_double(char *str, double *n)
 {
@@ -25,50 +42,56 @@ int	get_double(char *str, double *n)
 	return (0);
 }
 
-static int	parse_tokens_and_assign(char *str, double *a, double *b, double *c)
+static int	split_and_get_tokens(char *str, char **token1, char **token2,
+		char **token3)
 {
 	char	*comma1;
 	char	*comma2;
 
 	if (!str)
 		return (-1);
-	comma1 = str;
-	while (*comma1 && *comma1 != ',')
-		comma1++;
-	if (*comma1 != ',')
+	comma1 = ft_strchr(str, ',');
+	if (!comma1)
 		return (-1);
 	*comma1 = '\0';
-	comma2 = comma1 + 1;
-	while (*comma2 && *comma2 != ',')
-		comma2++;
-	if (*comma2 != ',')
+	comma2 = ft_strchr(comma1 + 1, ',');
+	if (!comma2)
 		return (-1);
 	*comma2 = '\0';
-	if (get_double(str, a) < 0 || get_double(comma1 + 1, b) < 0
-		|| get_double(comma2 + 1, c) < 0)
-		return (-1);
+	*token1 = str;
+	*token2 = comma1 + 1;
+	*token3 = comma2 + 1;
 	return (0);
 }
 
 int	parse_colors(char *str, t_color *color)
 {
-	double	r;
-	double	g;
-	double	b;
+	char	*r_str;
+	char	*g_str;
+	char	*b_str;
 
 	if (!str || !color)
 		return (-1);
-	if (parse_tokens_and_assign(str, &r, &g, &b) < 0)
+	if (split_and_get_tokens(str, &r_str, &g_str, &b_str) < 0)
 		return (-1);
-	color->red = (int)r;
-	color->green = (int)g;
-	color->blue = (int)b;
+	if (get_int(r_str, &color->red) < 0 || get_int(g_str, &color->green) < 0
+		|| get_int(b_str, &color->blue) < 0)
+		return (-1);
 	return (0);
 }
 
 int	parse_vec3(char *str, t_vec3 *vec)
 {
+	char	*x_str;
+	char	*y_str;
+	char	*z_str;
+
 	if (!str || !vec)
 		return (-1);
-	return (parse_tokens_and_assign(str, &(vec->x), &(vec->y), &(vec->z)));
+	if (split_and_get_tokens(str, &x_str, &y_str, &z_str) < 0)
+		return (-1);
+	if (get_double(x_str, &vec->x) < 0 || get_double(y_str, &vec->y) < 0
+		|| get_double(z_str, &vec->z) < 0)
+		return (-1);
+	return (0);
 }
