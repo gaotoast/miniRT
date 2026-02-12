1c43b8fe0014e8c22b5d4284b6e77b062175434c
shorei <160503699+gaotoast@users.noreply.github.com>
2026-02-12 16:54:08 +0900

    [fix] get_double関数を改善し、vec3とcolorのパース処理を修正

--- srcs/parsing/parse_camera.c
+++ srcs/parsing/parse_camera.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_camera.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kinamura <kinamura@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 13:29:46 by stakada           #+#    #+#             */
-/*   Updated: 2026/01/10 19:19:58 by kinamura         ###   ########.fr       */
+/*   Updated: 2026/02/12 16:47:52 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,8 @@ static char	*check_camera_format(char **elems, t_camera *camera, int read_flags)
 	if (!is_valid_csv(elems[1]) || parse_vec3(elems[1],
 			&(camera->direction)) < 0)
 		return (ERR_MSG_ELEM_FORMAT);
-	if (!is_valid_num(elems[2]) || get_double(elems[2], &(camera->fov)) < 0)
+	if (!is_valid_num(elems[2]) || get_double(elems[2], &(camera->fov),
+			NULL) < 0)
 		return (ERR_MSG_ELEM_FORMAT);
 	return (NULL);
 }
--- srcs/parsing/parse_utils.c
+++ srcs/parsing/parse_utils.c
@@ -6,92 +6,63 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 13:13:12 by stakada           #+#    #+#             */
-/*   Updated: 2026/02/11 23:56:42 by stakada          ###   ########.fr       */
+/*   Updated: 2026/02/12 16:47:59 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-static int	get_int(char *str, int *n)
+static int	get_int(const char *str, int *num, char **endptr)
 {
-	char		*endptr;
 	long long	ret;
 
-	if (!str || !*str || !n)
-		return (-1);
-	endptr = NULL;
-	ret = ft_strtol(str, &endptr, 10);
-	if (endptr && !(*endptr == '\0' || *endptr == '\n'))
-		return (-1);
+	ret = ft_strtol(str, endptr, 10);
 	if (ret < (long long)INT_MIN || ret > (long long)INT_MAX)
 		return (-1);
-	*n = (int)ret;
+	*num = (int)ret;
 	return (0);
 }
 
-int	get_double(char *str, double *n)
+int	get_double(const char *str, double *num, char **endptr)
 {
-	char	*endptr;
+	char	*tmp;
 
-	if (!str || !*str || !n)
+	*num = ft_strtod(str, &tmp);
+	if (endptr)
+		*endptr = tmp;
+	else if (tmp && !(*tmp == '\0' || *tmp == '\n'))
 		return (-1);
-	endptr = NULL;
-	*n = ft_strtod(str, &endptr);
-	if (endptr && !(*endptr == '\0' || *endptr == '\n'))
-		return (-1);
-	return (0);
-}
-
-static int	split_and_get_tokens(char *str, char **token1, char **token2,
-		char **token3)
-{
-	char	*comma1;
-	char	*comma2;
-
-	if (!str)
-		return (-1);
-	comma1 = ft_strchr(str, ',');
-	if (!comma1)
-		return (-1);
-	*comma1 = '\0';
-	comma2 = ft_strchr(comma1 + 1, ',');
-	if (!comma2)
-		return (-1);
-	*comma2 = '\0';
-	*token1 = str;
-	*token2 = comma1 + 1;
-	*token3 = comma2 + 1;
 	return (0);
 }
 
 int	parse_colors(char *str, t_color *color)
 {
-	char	*r_str;
-	char	*g_str;
-	char	*b_str;
+	char	*p;
 
 	if (!str || !color)
 		return (-1);
-	if (split_and_get_tokens(str, &r_str, &g_str, &b_str) < 0)
+	p = str;
+	if (get_int(p, &color->red, &p) < 0 || *p++ != ',')
 		return (-1);
-	if (get_int(r_str, &color->red) < 0 || get_int(g_str, &color->green) < 0
-		|| get_int(b_str, &color->blue) < 0)
+	if (get_int(p, &color->green, &p) < 0 || *p++ != ',')
+		return (-1);
+	if (get_int(p, &color->blue, &p) < 0 || (*p && *p != '\n'))
 		return (-1);
 	return (0);
 }
 
 int	parse_vec3(char *str, t_vec3 *vec)
 {
-	char	*x_str;
-	char	*y_str;
-	char	*z_str;
+	char	*p;
 
 	if (!str || !vec)
 		return (-1);
-	if (split_and_get_tokens(str, &x_str, &y_str, &z_str) < 0)
+	p = str;
+	if (get_double(p, &vec->x, &p) < 0 || *p++ != ',')
 		return (-1);
-	if (get_double(x_str, &vec->x) < 0 || get_double(y_str, &vec->y) < 0
-		|| get_double(z_str, &vec->z) < 0)
+	if (get_double(p, &vec->y, &p) < 0 || *p++ != ',')
+		return (-1);
+	if (get_double(p, &vec->z, &p) < 0 || (*p && *p != '\n'))
 		return (-1);
 	return (0);
 }
