2ba581674dc2163643d656db9c176753e180fbcc
shorei <160503699+gaotoast@users.noreply.github.com>
2026-02-13 23:59:20 +0900

    [refactor] compute_channel関数を追加し、shade_color関数を簡素化

--- srcs/render/render_shading.c
+++ srcs/render/render_shading.c
@@ -6,7 +6,7 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 00:41:16 by kinamura          #+#    #+#             */
-/*   Updated: 2026/02/13 21:38:06 by stakada          ###   ########.fr       */
+/*   Updated: 2026/02/13 23:44:05 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,32 +38,32 @@ int	get_background_color(t_scene *scene)
 	return (trgb_from_rgb(r, g, b));
 }
 
-static double	channel_intensity(double ambient, double light, double diffuse)
+static double	compute_channel(double obj_color, double ambient_color,
+		double light_color, double ambient_ratio, double diffuse)
 {
-	return (clamp01(ambient + light * diffuse));
+	double	ambient;
+	double	light;
+
+	ambient = ambient_ratio * ambient_color / 255.0;
+	light = light_color / 255.0;
+	return (obj_color * clamp01(ambient + light * diffuse));
 }
 
 int	shade_color(t_scene *scene, t_rgb object_color, double diffuse)
 {
-	double	ambient_r;
-	double	ambient_g;
-	double	ambient_b;
-	double	light_r;
-	double	light_g;
-	double	light_b;
+	double	r;
+	double	g;
+	double	b;
 
 	if (!scene)
 		return (0);
 	if (diffuse < 0.0)
 		diffuse = 0.0;
-	ambient_r = scene->ambient.ratio * scene->ambient.color.red / 255.0;
-	ambient_g = scene->ambient.ratio * scene->ambient.color.green / 255.0;
-	ambient_b = scene->ambient.ratio * scene->ambient.color.blue / 255.0;
-	light_r = scene->light.color.red / 255.0;
-	light_g = scene->light.color.green / 255.0;
-	light_b = scene->light.color.blue / 255.0;
-	return (trgb_from_rgb(object_color.red * channel_intensity(ambient_r,
-				light_r, diffuse), object_color.green
-			* channel_intensity(ambient_g, light_g, diffuse), object_color.blue
-			* channel_intensity(ambient_b, light_b, diffuse)));
+	r = compute_channel(object_color.red, scene->ambient.color.red,
+			scene->light.color.red, scene->ambient.ratio, diffuse);
+	g = compute_channel(object_color.green, scene->ambient.color.green,
+			scene->light.color.green, scene->ambient.ratio, diffuse);
+	b = compute_channel(object_color.blue, scene->ambient.color.blue,
+			scene->light.color.blue, scene->ambient.ratio, diffuse);
+	return (trgb_from_rgb(r, g, b));
 }
